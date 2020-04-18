e7a6d75673da391269b9534c2fafa2cd8d11842f
Josue <35548140+josuecarames@users.noreply.github.com>
2020-04-18 18:57:08 +0000

    unit 4

---
+++ RCA/JS/Warm Up 3/warmup3.js
@@ -0,0 +1,26 @@
+let str = ["cat", "The fox", "Cow jumped over the moon", "good day"];
+
+const strLength = function (stringArr) {
+    for (i = 0; i <= 0; i++) {
+        
+        if (stringArr[i].length > i) {
+            console.log("Item " + i + " in the list is the longest item");
+        } 
+        
+
+        if (stringArr[i].length > i) {
+            console.log("Item " + i + " in the list is the longest item");
+        }
+    }
+}
+
+console.log(strLength(str));
+
+// Output: 21
+
+// "Cow jumped over the moon" = 24
+
+// "cat" = 3
+
+// return 24 - 3
+
